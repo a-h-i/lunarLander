e0edecd3a174cd19f804f7d4c0344356ba30a210
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2013-11-24 23:17:09 +0200

    Formatted ui shaders

--- shaders/ui/vertex.glsl
+++ shaders/ui/vertex.glsl
@@ -2,7 +2,7 @@
 layout (location = 0) in vec2 inVert;
 layout (location = 1) in vec4 inColor;
 uniform mat4 mvp;
-smooth out vec4 color;
+flat out vec4 color;
 
 void main() {
     vec4 homoVect = vec4(inVert, 0.0f, 1.0f);
